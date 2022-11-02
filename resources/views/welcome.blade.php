6af44c624657bcedd87cecc911347b2c4191c380
thiagomrvieira <thiagomrvieira@gmail.com>
2022-11-02 20:17:29 +0000

    Counters and Namespacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,14 @@
         <title>Laravel</title>
     </head>
     <body>
-        <h1> Hello you're the visitor number #{{ $visits }}</h1>
+        {{-- Lesson 01 - Visitor count --}}
+        {{-- <h1> Hello you're the visitor number #{{ $visits }}</h1> --}}
+
+        {{-- Lesson 02 - Namespacing --}}
+        <h1>Some videos</h1>
+
+        <p>
+            This video has been downloaded {{ $downloads ?? 0 }} times
+        </p>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,22 @@
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Lesson 01 - Visitor count
+// Route::get('/', function () {
+//     $visits =  Redis::incr('visits');
+//     return view('welcome', ['visits' => $visits]);
+//     return view('welcome')->withVisits($visits);
+// });
 
-Route::get('/', function () {
-    $visits =  Redis::incr('visits');
+// Lesson 02 - Namespacing
+Route::get('/videos/{id}', function ($id) {
+    $downloads =  Redis::get("videos.{$id}.downloads");
 
-    // return view('welcome', ['visits' => $visits]);
-    return view('welcome')->withVisits($visits);
+    return view('welcome')->withDownloads($downloads);
+});
+
+Route::get('/videos/{id}/download', function ($id) {
+    $downloads =  Redis::incrBy("videos.{$id}.downloads", 1);
+
+    return back();
 });
